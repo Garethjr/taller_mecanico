5926657871d75ee8ae9af534f826e9818a7a6166
Muñoz Tomas <tomas.gem464@gmail.com>
2025-04-21 12:20:30 -0300

    update - develop

--- taller_api/config.py
+++ taller_api/config.py
@@ -1,1 +1,15 @@
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/taller_api"
+from dotenv import load_dotenv
+import os
+
+# Carga las variables del archivo .env
+load_dotenv()
+
+#Obtener valores
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DB = os.getenv("MYSQL_DB")
+
+# Arma la URI de conexion
+
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
