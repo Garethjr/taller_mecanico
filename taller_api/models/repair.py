664ee3210d13f8d29b99eb4d7058bfb15fd3f89a
Muñoz Tomas <tomas.gem464@gmail.com>
2025-04-24 16:44:12 -0300

    update

--- taller_api/models/repair.py
+++ taller_api/models/repair.py
@@ -1,6 +1,8 @@
 from database.db import db
+
 class Repair (db.Model):
     _tablename_ = 'repair'
+
     id = db.Column(db.Integer, primary_key=True)
     descripcion = db.Column(db.String(200), nullable=False)
     fecha = db.Column(db.Date, nullable=False)
