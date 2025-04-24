ec5b61858b349619e11abd44809977cc2f0c154d
Muñoz Tomas <tomas.gem464@gmail.com>
2025-04-24 11:39:04 -0300

    Create seed

---
+++ taller_api/seed.py
@@ -0,0 +1,50 @@
+import json
+from datetime import datetime
+from flask import Flask
+from database import db
+from models.client import Client
+from models.car import Car
+from app import app
+
+# Iniciamos el contexto de la app para que SQLAlchemy pueda trabajar con la base de datos.
+with app.app_context():
+    try:
+        # Cargamos clientes desde el archivo json
+        with open ("data_json/datos_client.json", encoding="utf-8") as file:
+            clients_data = json.load(file) # Convertimos el JSON en una lista de diccionarios
+
+            for client in clients_data:
+                # Verificamos si ya hay un cliente con ese correo.
+                if not Client.query.filter_by(email = client["email"]).fisrt():
+                    # Creamos un nuevo cliente y lo agregamos a la base de datos
+                    new_client = Client(
+                        name =client["name"],
+                        email =client["email"],
+                        phone =client["phone"],
+                        birthsate =datetime.strptime(client["birthdate"], "%Y-%m-%d").date()
+                    )
+                    db.session.add(new_client) # Preparamos al cliente para ser agreagado a la base de datos
+
+            # Cargamos los autos del archivo json
+            with open("data_json/datos_car.json", encoding="utf-8") as file:
+                cars_data = json.load(file)
+
+                for car in cars_data:
+                    # Verificamos si hay un auto con esa patente
+                    if not Car.query.filter_by(license_plate=car["license_plate"]).first():
+                        # Creamos un nuevo auto y lo agregamos a la base de datos
+                        new_car = Car(
+                            brand =car["brand"],
+                            model =car["model"],
+                            year =car["year"],
+                            patent =car["patent"],
+                            client_id=car["client_id"],
+                        )
+                        db.session.add(new_car) # Preparamos el auto para ser agreagado a la base de datos
+            
+            db.session.commit() # Guardamos los cmabios en la base de datos
+            print("Los datos fueron cargados correctamente")
+
+    except Exception as e:
+        db.session.rollback() # Cncela todo si hay un error
+        print(f"Error al cargar datos: {e}")
