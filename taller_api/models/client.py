6c9c15d43fc6b8dbdd2755d1542932f3d9390a62
Leandro Briceño <leagustin9@gmail.com>
2025-04-24 15:23:24 -0300

    add client model
    agrego el modelo cliente actualizado

---
+++ taller_api/models/client.py
@@ -0,0 +1,37 @@
+from database import db
+from datetime import date
+from datetime import datetime
+
+class Client(db.Model):
+    _tablename_="client"
+
+    id=db.column(db.Integer,primary_key=True)
+    name=db.column(db.String(40),nullable=True)
+    email=db.column(db.String(200),unique=True,nullable=True)
+    phone=db.column(db.String(20),unique=True,nullable=True)
+    birthdate=db.column(db.string(40),nullable=True)
+
+    def _init_(self,name,email,phone,birtdhate):
+        self.name=name
+        self.email=email
+        self.phone=phone
+        self.birthdate=birtdhate
+    
+    def get_age(self):
+        if self.birthdate:
+            fecha_str=self.birthdate
+            fecha_date=datetime.strptime(fecha_str,"%Y/%m/%d").date()
+            today=date.today()#guardamos la fecha de hoy
+            age=today.year-fecha_date.year
+            if(today.month,today.day)<(fecha_date.month,fecha_date.day):#saber si ya cumplió años
+                age-=1
+            return age
+        return None
+    
+    def serialize(self):
+        return{
+            "name": self.name,
+            "email":self.email,
+            "phone":self.phone,
+            "age":self.get_age()
+        }
