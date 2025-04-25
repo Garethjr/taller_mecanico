675edd5c27ed26f7ef53dffcb01ca7109911c56d
Muñoz Tomas <tomas.gem464@gmail.com>
2025-04-25 17:47:52 -0300

    update

--- taller_api/routes/__init__.py
+++ taller_api/routes/__init__.py
@@ -1,3 +1,4 @@
-from .clients import *
-from .cars import *
-from .repairs import *
+from .clients import clients_bp
+from .cars import cars_bp
+from .repairs import repairs_bp
+from .mechanic import mechanic_bp
