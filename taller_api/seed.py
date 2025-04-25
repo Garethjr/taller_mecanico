c783656e7d466d1f77276c149b82e36ebc0273ef
Leandro Briceño <leagustin9@gmail.com>
2025-04-24 23:22:10 -0300

    seed modification
    modificando seed del cliente

--- taller_api/seed.py
+++ taller_api/seed.py
@@ -15,32 +15,36 @@ with app.app_context():
 
             for client in clients_data:
                 # Verificamos si ya hay un cliente con ese correo.
-                if not Client.query.filter_by(email = client["email"]).fisrt():
+                if not Client.query.filter_by(
+                    (Client.email == client["email"]) | (Client.phone == client["phone"])
+                    ).first():
                     # Creamos un nuevo cliente y lo agregamos a la base de datos
                     new_client = Client(
                         name =client["name"],
                         email =client["email"],
                         phone =client["phone"],
-                        birthsate =datetime.strptime(client["birthdate"], "%Y-%m-%d").date()
+                        birthdate =datetime.strptime(client["birthdate"], "%Y-%m-%d").date()
                     )
                     db.session.add(new_client) # Preparamos al cliente para ser agreagado a la base de datos
+            db.session.commit() # Guardamos los cambios en la base de datos
+            print("--Datos del cliente cargados--")
 
             # Cargamos los autos del archivo json
-            with open("data_json/datos_car.json", encoding="utf-8") as file:
-                cars_data = json.load(file)
+        with open("data_json/datos_car.json", encoding="utf-8") as file:
+            cars_data = json.load(file)
 
-                for car in cars_data:
-                    # Verificamos si hay un auto con esa patente
-                    if not Car.query.filter_by(license_plate=car["license_plate"]).first():
-                        # Creamos un nuevo auto y lo agregamos a la base de datos
-                        new_car = Car(
-                            brand =car["brand"],
-                            model =car["model"],
-                            year =car["year"],
-                            patent =car["patent"],
-                            client_id=car["client_id"],
-                        )
-                        db.session.add(new_car) # Preparamos el auto para ser agreagado a la base de datos
+            for car in cars_data:
+                # Verificamos si hay un auto con esa patente
+                if not Car.query.filter_by(license_plate=car["license_plate"]).first():
+                    # Creamos un nuevo auto y lo agregamos a la base de datos
+                    new_car = Car(
+                        brand =car["brand"],
+                        model =car["model"],
+                        year =car["year"],
+                        patent =car["patent"],
+                        client_id=car["client_id"],
+                    )
+                    db.session.add(new_car) # Preparamos el auto para ser agreagado a la base de datos
             
             db.session.commit() # Guardamos los cmabios en la base de datos
             print("Los datos fueron cargados correctamente")
